79e432bc3f99a3dda68771aa08c025f7f858bf70
bunchgrape <bqfu21@cse.cuhk.edu.hk>
2026-08-20 17:40:52 +0800

    update build config

---
+++ update.sh
@@ -0,0 +1,5 @@
+git -C frontends/slang/lib apply  ../../../patches/cueda/frontends-slang-lib.patch
+git -C libs/cxxopts apply  ../../patches/cueda/cxxopts.patch
+git -C libs/fmt apply  ../../patches/cueda/fmt.patch
+git -C libs/slang apply  ../../patches/cueda/slang.patch
+git -C libs/tomlplusplus apply  ../../patches/cueda/tomlplusplus.patch
